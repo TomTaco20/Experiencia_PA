0327e2138df5692ff413ff53f10d1121e2d0becd
ClaudioLeyton <163364313+ClaudioLeyton@users.noreply.github.com>
2024-03-14 16:28:29 +0000

    memoria ready

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,4 @@
+import random
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +6,41 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    print("ELIGE LA DIFICULTAD (FACIL, NORMAL, DIFICIL, JORGE)")
+    dificultad = str(input()).upper()
+    if dificultad == "FACIL":
+        longitud = random.randint(2,4)
+    elif dificultad == "NORMAL":
+        longitud = random.randint(5,7)
+    elif dificultad == "DIFICIL":
+        longitud = random.randint(8,10)
+    elif dificultad == "JORGE":
+        longitud = random.randint(11,20)
+    else:
+        print("pusiste mal la dificultad...")
+        longitud = random.randint(20,30)
+
+    print("RECUERDA ESTOS NUMEROS!!!!")
+    lista1 = ""
+    lista2 = ""
+    for i in range(longitud):
+        numero = random.randint(0,9)
+        lista1 += str(numero) + ","
+        lista2 += str(numero)
+    lista1 = lista1[:len(lista1)-1]
+    print(lista1)
+    print("Apreta Enter para comenzar")
+    pregunta = input()
+    for i in range(20):
+        print(".\n\n\n\n")
+    print("Escribe los numeros")
+
+    intento = str(input())
+
+    while (intento != lista1) and (intento != lista2):
+        print("Nop, inténtalo de nuevo")
+        intento = str(input())
+
+    print("Lo lograste, tienes muy buena memoria")
+
+memoria()
