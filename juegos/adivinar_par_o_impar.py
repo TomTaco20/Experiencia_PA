922830f89892459ad5449426c86bc8239080bdda
ClaudioLeyton <163364313+ClaudioLeyton@users.noreply.github.com>
2024-03-14 13:35:35 +0000

    tamo

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,12 +1,6 @@
 import random
 opcion = input('¿El numero sera par o impar? : ')
 def adivinar_par_o_impar(opcion):
-    """
-    Esta función representa el juego de adivinar si un número es par o impar.
-    Tienes que permitir que el usuario ingrese una de las dos opciones y
-    generar un número aleatorio para ver si es par o impar.
-    Se debe mostrar si el usuario adivina correctamente o no.
-    """
     numero = random.randint(1,1000)
     if opcion == "impar" and numero%2 != 0:
         return f"wena ganaste, el numero era: {numero}"
