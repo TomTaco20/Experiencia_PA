ec88f3da6570d9007cb99a710325cac778beefbb
TomTaco20 <163323513+TomTaco20@users.noreply.github.com>
2024-03-14 17:17:44 +0000

    Tamo listo

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -6,13 +6,14 @@ def juego_del_dado():
         if jp <= 30 or pc <= 30:
             n1=random.randint(1,6)
             jp+= n1
-
+            print(f'{n1} valor del dado del jugador. Puntaje actualjugador: {jp}')
             n2=random.randint(1,6)
             pc+= n2
+            print(f'{n2} valor del dado de la computadora. Puntaje actual PC: {pc}')
             if jp >= 30:
-                return(print('Has ganado!'))
+                return(print(f'Has ganado con {jp} puntos'))
             elif pc >= 30:
-                return(print('Ha ganado el pc!'))
+                return(print(f'Ha ganado el pc con {pc} puntos'))
     return('elpepe')
 input('Presiona Enter para comenzar')
 juego_del_dado()
