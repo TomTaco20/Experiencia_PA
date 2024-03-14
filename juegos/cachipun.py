c104a2c034ffa5521ba01b2e0dd7d22a17d16ab8
ClaudioLeyton <163364313+ClaudioLeyton@users.noreply.github.com>
2024-03-14 15:38:52 +0000

    finisheado

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,17 +1,25 @@
 import random
 jugada = input("Vamos a jugar al cachipun, veamos si puedes vencer a una computadora (si pierdes seras reemplazado por una), elige entre piedra, papel o tijera : ")
 def cachipun(jugada):
-    """
-    Esta función representa el juego de cachipun.
-    Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
-    La computadora debe elegir una opción al azar.
-    """
     jugadas_pc = ["piedra","papel","tijera"]
     eleccion_pc = random.randint(0,2)
     if jugada == jugadas_pc[eleccion_pc]:
         return "uff, empate, te haz salvado"
-    elif jugadas_pc[eleccion_pc] == "papel":
-        return "haz perdido, ahora moriras (en realidad no)"
-    elif jugadas_pc[eleccion_pc] == "tijera":
-        return "felicidades, haz ganado, no serás reemplazado por una IA"
+    elif jugada == "piedra":
+        if jugadas_pc[eleccion_pc] == "papel":
+            return f"perdiste, jugada computadora: {jugadas_pc[eleccion_pc]}"
+        elif jugadas_pc[eleccion_pc] == "tijera":
+            return f"ganaste, jugada computadora: {jugadas_pc[eleccion_pc]}"
+    elif jugada == "papel":
+        if jugadas_pc[eleccion_pc] == "tijera":
+            return f"perdiste, jugada computadora: {jugadas_pc[eleccion_pc]}"
+        elif jugadas_pc[eleccion_pc] == "piedra":
+            return f"ganaste, jugada computadora: {jugadas_pc[eleccion_pc]}"
+    elif jugada == "tijera":
+        if jugadas_pc[eleccion_pc] == "piedra":
+            return f"perdiste, jugada computadora: {jugadas_pc[eleccion_pc]}"
+        elif jugadas_pc[eleccion_pc] == "papel":
+            return f"ganaste, jugada computadora: {jugadas_pc[eleccion_pc]}"
+print(cachipun(jugada))
+    
     
